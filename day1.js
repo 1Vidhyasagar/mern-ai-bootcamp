5253d52d2a5cf2e5684c7b4a266de123161a2d39
Vidhyasagar <vidhyasagarmyana9598@gmail.com>
2026-05-05 15:23:28 +0530

    Day 1: JavaScript basics with array and object

---
+++ day1.js
@@ -0,0 +1,25 @@
+let age = 25; // number
+const name = "Vidhyasagar"; // string
+let isLearning = true; // boolean
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Learning:", isLearning);
+
+// Operators
+let sum = 10 + 5;
+let product = 10 * 5;
+console.log("Sum:", sum);
+console.log("Product:", product);
+
+// Challenge
+// Add two more variables: one array and one object.
+let hobbies = ["Watching movies", " Fitness", " Travelling"];
+let myCar = {
+    Brand: "Volkswagon POLO",
+    FuelType: "Diesel",
+    Year:"2016"
+}
+
+// Print them using console.log.
+console.log("Hi, My hobbies are " + hobbies + " and I have " + myCar.Brand + " car")
